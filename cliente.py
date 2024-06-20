426588389c18ab1adb31b48c9b2a1a1ac053c6ca
JMateoG <71849001+JMateoG@users.noreply.github.com>
2024-06-20 16:52:33 -0500

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,53 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import time
+import json
+from datetime import datetime
+
+spark = SparkSession \
+  .builder \
+  .appName("streaming") \
+  .master("local[*]") \
+  .getOrCreate()
+
+schema = ArrayType(StructType([
+    StructField("object_id", IntegerType(), True),
+    StructField("latitude", DoubleType(), True),
+    StructField("longitude", DoubleType(), True),
+    StructField("date", StringType(), True),
+    StructField("customer_id", IntegerType(), True),
+    StructField("employee_id", IntegerType(), True),
+    StructField("quantity_products", IntegerType(), True),
+    StructField("order_id", StringType(), True)
+]))
+
+static_schema = StructType([
+    StructField("object_id", IntegerType(), True),
+    StructField("latitude", DoubleType(), True),
+    StructField("longitude", DoubleType(), True),
+    StructField("date", StringType(), True),
+    StructField("customer_id", IntegerType(), True),
+    StructField("employee_id", IntegerType(), True),
+    StructField("quantity_products", IntegerType(), True),
+    StructField("order_id", StringType(), True)
+])
+
+static_df = spark.createDataFrame([], static_schema)
+while True:
+    streaming_df = spark.readStream.format("socket").option("host", "localhost").option("port", "8000").load()
+    json_df = streaming_df.select(from_json(col("value"), schema).alias("data"))
+    json_df = json_df.selectExpr("explode(data) as dict").select("dict.*")
+    writing_df = json_df.writeStream.format("memory").queryName("socketData").outputMode("update").start()
+    static_stream_df = spark.sql("SELECT * FROM socketData")
+    static_df = static_df.union(static_stream_df)
+    writing_df.awaitTermination(2)
+    writing_df.stop()
+    if static_df.count() > 50:
+        now = datetime.now()
+        now = now.strftime("%d_%m_%Y__%H_%M_%S")
+        ruta = '/raw/new/data_raw_'+now+'.parquet'
+        print(ruta)
+        static_df.write.mode('append').parquet(ruta)
+        static_df = spark.createDataFrame([], static_schema)
+    time.sleep(30)
--- servidor.py
+++ servidor.py
@@ -5,27 +5,42 @@ from shapely.geometry import Point
 import datetime
 import socket
 import json
-import time
 
 # Paths de los archivos
 gdf_path = '50001.parquet'
 customers_path = 'customers.parquet'
 employees_path = 'employees.parquet'
+neighborhood_path ='medellin_neighborhoods.parquet'
 
 # Leer datos
 def cargar_datos():
     gdf_medellin = gpd.read_parquet(gdf_path)
     df_customers = pd.read_parquet(customers_path)
     df_employees = pd.read_parquet(employees_path)
-    return gdf_medellin, df_customers, df_employees
+    gdf_neighborhood = gpd.read_parquet(neighborhood_path)
+    return gdf_medellin, df_customers, df_employees, gdf_neighborhood
 
 # Generar coordenadas aleatorias dentro de Medellín
-def generar_coordenadas(poligono):
+def generar_coordenadas(poligono,gdf_neighborhood):
     min_x, min_y, max_x, max_y = poligono.bounds
     while True:
         punto_aleatorio = Point(random.uniform(min_x, max_x), random.uniform(min_y, max_y))
         if poligono.contains(punto_aleatorio):
-            return punto_aleatorio.x, punto_aleatorio.y
+            punto = Point(punto_aleatorio.x, punto_aleatorio.y)
+            filtro = gdf_neighborhood['geometry'].geometry.contains(punto)
+            poligono_encontrado = gdf_neighborhood[filtro]
+            if not poligono_encontrado.empty:
+                return punto_aleatorio.x, punto_aleatorio.y
+
+#Generar el barrio y la comuna donde estan las coordendas         
+def generar_comuna(longitude, latitude, gdf_neighborhood):
+    punto = Point(longitude, latitude)
+    filtro = gdf_neighborhood['geometry'].geometry.contains(punto)
+    poligono_encontrado = gdf_neighborhood[filtro]
+    if not poligono_encontrado.empty:
+        return poligono_encontrado.iloc[0]['OBJECTID']
+    else:
+        return None
 
 # Obtener siguiente carácter en un string
 def siguiente_caracter(caracter):
@@ -58,8 +73,9 @@ def aumentar_codigo(codigo):
     return new_codigo
 
 # Crear datos simulados
-def crear_datos(gdf_medellin, df_customers, df_employees, mu, sigma, order):
-    longitude, latitude = generar_coordenadas(gdf_medellin.loc[0]['geometry'])
+def crear_datos(gdf_medellin, df_customers, df_employees, gdf_neighborhood, mu, sigma, order):
+    longitude, latitude = generar_coordenadas(gdf_medellin.loc[0]['geometry'], gdf_neighborhood)
+    object_id = int(generar_comuna(longitude, latitude, gdf_neighborhood))
     date_actual = datetime.datetime.now()
     date_formateada = date_actual.strftime("%d/%m/%Y %H:%M:%S")
     customer_size = df_customers['customer_id'].size
@@ -71,6 +87,7 @@ def crear_datos(gdf_medellin, df_customers, df_employees, mu, sigma, order):
     quantity_products = int(random.gauss(mu, sigma))
     order_id = aumentar_codigo(order)
     datos = {
+        "object_id": object_id,
         "latitude": latitude,
         "longitude": longitude,
         "date": date_formateada,
@@ -89,10 +106,10 @@ def iniciar_servidor():
     return mi_socket
 
 # Generar datos y enviar al cliente
-def enviar_datos(socket_cliente, gdf_medellin, df_customers, df_employees, mu, sigma, order_base):
+def enviar_datos(socket_cliente, gdf_medellin, df_customers, df_employees, gdf_neighborhood, mu, sigma, order_base):
     list_data = []
     for _ in range(random.randint(5, 10)):  # Genera más de 5 datos
-        data = crear_datos(gdf_medellin, df_customers, df_employees, mu, sigma, order_base)
+        data = crear_datos(gdf_medellin, df_customers, df_employees, gdf_neighborhood, mu, sigma, order_base)
         order_base = data['order_id']
         list_data.append(data)
     json_data = json.dumps(list_data, default=str)  # Convertir datetime a string antes de serializar
@@ -103,13 +120,13 @@ def enviar_datos(socket_cliente, gdf_medellin, df_customers, df_employees, mu, s
 
 # Ejecutar servidor
 def ejecutar_servidor():
-    gdf_medellin, df_customers, df_employees = cargar_datos()
+    gdf_medellin, df_customers, df_employees, gdf_neighborhood = cargar_datos()
     order_base = 'd8b9b417-b098-4344-b137-362894e4dccb'
     mu, sigma = 500, 175
     mi_socket = iniciar_servidor()
     while True:
         conexion, addr = mi_socket.accept()
-        order_base = enviar_datos(conexion, gdf_medellin, df_customers, df_employees, mu, sigma, order_base)
+        order_base = enviar_datos(conexion, gdf_medellin, df_customers, df_employees, gdf_neighborhood, mu, sigma, order_base)
         print("Envio exitoso:",order_base)
 
 # Ejecutar servidor
